2d0c7f4e8030482fcf1f2712a29bff75275906a5
Mohannad Khrata <20096866@mail.wit.ie>
2025-08-09 17:30:35 +0100

    Tasky Lab 2a - Exercise done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 
 const Task = (props) => {
-    
-   
-
-    return (
-        <div className="card">
-            <p className="title">{props.title}</p>
-            <p>Due: {props.deadline}</p>
-            <p>{props.children}</p>
-        </div>
-    )
-
-
-}
-
+  return (
+    <div className="card">
+      <p className="title">{props.title}</p>
+      <p>Due: {props.deadline}</p>
+      {props.description && <p className="desc">{props.description}</p>}
+      <p className={`priority ${props.priority}`}>{props.priority}</p>
+    </div>
+  );
+};
 
 export default Task;
